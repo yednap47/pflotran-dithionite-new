8509cfd82f987f6630c2cb9e398855a2d3a99e0a
Sachin Pandey <sach@lanl.gov>
2017-11-08 15:28:12 -0700

    Updates.

--- src/pflotran/reaction_sandbox.F90
+++ src/pflotran/reaction_sandbox.F90
@@ -8,12 +8,8 @@ module Reaction_Sandbox_module
   use Reaction_Sandbox_Cyber_class
 
   ! Add new reacton sandbox classes here.
-  use Reaction_Sandbox_S2o4_fe3_class
-  use Reaction_Sandbox_S2o4_o2_class
-  use Reaction_Sandbox_S2o4_disp_class
-  use Reaction_Sandbox_Fe2_o2_class
-  use Reaction_Sandbox_Fe2_cr6_class
   use Reaction_Sandbox_Dithionite_class
+  use Reaction_Sandbox_Dithionite_v2_class
 
   use PFLOTRAN_Constants_module
 
@@ -169,18 +165,10 @@ subroutine RSandboxRead2(local_sandbox_list,input,option)
         new_sandbox => SimpleCreate()
       case('CYBERNETIC')
         new_sandbox => CyberCreate()
-      case('S2O4_FE3')
-        new_sandbox => S2o4_fe3Create()
-      case('S2O4_O2')
-        new_sandbox => S2o4_o2Create()
-      case('S2O4_DISP')
-        new_sandbox => S2o4_dispCreate()
-      case('FE2_O2')
-        new_sandbox => Fe2_o2Create()
-      case('FE2_CR6')
-        new_sandbox => Fe2_cr6Create()
       case('DITHIONITE_PARAMETERS')
         new_sandbox => DithioniteCreate()
+      case('DITHIONITE_V2_PARAMETERS')
+        new_sandbox => Dithionite_v2Create()
       case default
         call InputKeywordUnrecognized(word,'CHEMISTRY,REACTION_SANDBOX',option)
     end select
